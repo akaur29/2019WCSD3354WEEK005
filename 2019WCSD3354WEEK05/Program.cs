cbacc16f942b4a5cc74dc9a1f2cb8168360fc60b
visualstudio <visualstudio@230-PC43>
2019-02-07 14:11:24 -0500

    commitat2:11

--- 2019WCSD3354WEEK05/Program.cs
+++ 2019WCSD3354WEEK05/Program.cs
@@ -4,12 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _2019WCSD3354WEEK05
+namespace ConsoleApp3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var a = new TestQuestion2();
+            a.PlayingWithForLoops();
+        }
+        class TestQuestion2
+        {
+            public void PlayingWithForLoops()
+            {
+                for (; mymethod();)
+                {
+                    if (myfavouritevariables > 10) {
+                        Console.WriteLine("i am in for loops");
+                        break;
+                    }
+                }
+                Console.WriteLine("oh no i have to go through this stupoid loop again");
+            }
+        }
+            public bool mymethod()
+            {
+            myfavouritevariables
+                return true;
+            }
         }
     }
 }
